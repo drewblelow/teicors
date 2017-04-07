367ecbe1a4a83ebae78cbb69714f61786fa3884f
Nicholas Low <niggleslow@gmail.com>
2017-04-08 00:08:57 +0800

    added index content, changed menu button, media query now using css!

---
+++ js/indexcontent.js
@@ -0,0 +1,69 @@
+function renderContent(parentId) {
+    var parentDiv = document.getElementById(parentId);
+    console.log(parentDiv);
+    parentDiv.className = "content-container";
+    parentDiv.width = "100%";
+    
+    //Welcome: blah blah
+    var welcomeDiv = document.createElement("h2");
+    welcomeDiv.className = "content-container-welcome";
+    
+    var welcomeText = document.createTextNode("Welcome Juan Lopez!");
+    welcomeDiv.appendChild(welcomeText);
+    
+    parentDiv.appendChild(welcomeDiv);
+    
+    // Verify Personal Data:
+    var verifyDiv = document.createElement("h3");
+    verifyDiv.className = "content-container-verify";
+    
+    var verifyText = document.createTextNode("Verify Personal Data:");
+    verifyDiv.appendChild(verifyText);
+    
+    parentDiv.appendChild(verifyDiv);
+    
+    // Personal Data
+    var personaldataDiv = document.createElement("P");
+    personaldataDiv.className = "content-container-personaldata";
+    
+    var linebreak1 = document.createElement("br");
+    var linebreak2 = document.createElement("br");
+    var linebreak3 = document.createElement("br");
+    var linebreak4 = document.createElement("br");
+    var matriculationnumText = document.createTextNode("Matriculation Number: A0101010B");
+    var nameText = document.createTextNode("Name: Juan Lopez");
+    var courselevelText = document.createTextNode("Course Level: Computing 1");
+    var degreeText = document.createTextNode("Degree: Bachelor of Computing with Honours");
+    var academicyearText = document.createTextNode("Academic Year: 2016/2017");
+    
+    personaldataDiv.appendChild(matriculationnumText);
+    personaldataDiv.appendChild(linebreak1);
+    personaldataDiv.appendChild(nameText);
+    personaldataDiv.appendChild(linebreak2);
+    personaldataDiv.appendChild(courselevelText);
+    personaldataDiv.appendChild(linebreak3);
+    personaldataDiv.appendChild(degreeText);
+    personaldataDiv.appendChild(linebreak4);
+    personaldataDiv.appendChild(academicyearText);
+    
+    parentDiv.appendChild(personaldataDiv);
+    
+    // Help 
+    var helpDiv = document.createElement("P");
+    helpDiv.className = "content-container-help";
+    
+    var helpText = document.createTextNode("Note: Need help with the new interface? Click here for the user guide!");
+    helpDiv.appendChild(helpText);
+    
+    parentDiv.appendChild(helpDiv);
+    
+    // More Info
+    var moreinfoDiv = document.createElement("P");
+    moreinfoDiv.className = "content-container-info";
+    
+    var moreinfoText = document.createTextNode("For more information on declaration, please click Declaration link on the menu to the left.");
+    moreinfoDiv.append(moreinfoText);
+    
+    parentDiv.appendChild(moreinfoDiv);
+
+}
--- js/template.js
+++ js/template.js
@@ -9,118 +9,105 @@ function renderTopNav(parentId){
     logoDiv.className = "navbar-logo-container";
     var logoImage = document.createElement('img');
     logoImage.src = "../assets/img/partylion.png";
-    
-    var mq = window.matchMedia("(min-width: 500px)");
-    
-    if(mq.matches) {
-        logoImage.width = "213";
-        logoImage.height = "98";    
-    } else {
-        logoImage.width = "185";
-        logoImage.height = "75";
-    }
+    logoImage.className = "navbar-logo-pic";
     logoDiv.appendChild(logoImage);
     
-    console.log(mq);
-    if(mq.matches) {
-        var navbarDiv = document.createElement("span");
-        navbarDiv.className = "navbar-link-container";
+    var navbarDiv = document.createElement("span");
+    navbarDiv.className = "navbar-link-container";
     
-        var homeLink = document.createElement("a");
-        homeLink.href = "../docs/index.html";
-        homeLink.text = "Home";
-        homeLink.className = "navbar-links";
-        navbarDiv.appendChild(homeLink);
+    var homeLink = document.createElement("a");
+    homeLink.href = "../docs/index.html";
+    homeLink.text = "Home";
+    homeLink.className = "navbar-links";
+    navbarDiv.appendChild(homeLink);
 
-        var manageModuleLink = document.createElement("a");
-        manageModuleLink.href = "";
-        manageModuleLink.text = "Manage Modules";
-        manageModuleLink.className = "navbar-links";
-        navbarDiv.appendChild(manageModuleLink);
+    var manageModuleLink = document.createElement("a");
+    manageModuleLink.href = "";
+    manageModuleLink.text = "Manage Modules";
+    manageModuleLink.className = "navbar-links";
+    navbarDiv.appendChild(manageModuleLink);
         
-        var moduleBiddingLink = document.createElement("a");
-        moduleBiddingLink.href = "";
-        moduleBiddingLink.text = "Module Bidding";
-        moduleBiddingLink.className = "navbar-links";
-        navbarDiv.appendChild(moduleBiddingLink);
+    var moduleBiddingLink = document.createElement("a");
+    moduleBiddingLink.href = "";
+    moduleBiddingLink.text = "Module Bidding";
+    moduleBiddingLink.className = "navbar-links";
+    navbarDiv.appendChild(moduleBiddingLink);
 
-        var nusBidPointsLink = document.createElement("a");
-        nusBidPointsLink.href = "";
-        nusBidPointsLink.text = "NUS Bid Points";
-        nusBidPointsLink.className = "navbar-links";
-        navbarDiv.appendChild(nusBidPointsLink);
+    var nusBidPointsLink = document.createElement("a");
+    nusBidPointsLink.href = "";
+    nusBidPointsLink.text = "NUS Bid Points";
+    nusBidPointsLink.className = "navbar-links";
+    navbarDiv.appendChild(nusBidPointsLink);
         
-        var tutRegLink = document.createElement("a");
-        tutRegLink.href = "";
-        tutRegLink.text = "Tutorial Registration";
-        tutRegLink.className = "navbar-links";
-        navbarDiv.appendChild(tutRegLink);
+    var tutRegLink = document.createElement("a");
+    tutRegLink.href = "";
+    tutRegLink.text = "Tutorial Registration";
+    tutRegLink.className = "navbar-links";
+    navbarDiv.appendChild(tutRegLink);
 
-        var appealLink = document.createElement("a");
-        appealLink.href = "";
-        appealLink.text = "Appeal";
-        appealLink.className = "navbar-links";
-        navbarDiv.appendChild(appealLink);
+    var appealLink = document.createElement("a");
+    appealLink.href = "";
+    appealLink.text = "Appeal";
+    appealLink.className = "navbar-links";
+    navbarDiv.appendChild(appealLink);
+    
+    var buttonDiv = document.createElement("div");
+    buttonDiv.className = "dropdown";
         
-        parentDiv.appendChild(logoDiv);
-        parentDiv.appendChild(navbarDiv);
-    } else {
-        var buttonDiv = document.createElement("div");
-        buttonDiv.className = "dropdown";
+    var button = document.createElement("BUTTON");
+    button.className = "ddbtn";
+    buttonDiv.appendChild(button);
         
-        var button = document.createElement("BUTTON");
-        button.className = "ddbtn";
-        var buttonText = document.createTextNode("Menu");
-        buttonText.className = "btnText";
-        button.appendChild(buttonText);
-        buttonDiv.appendChild(button);
-        
-        var menuLinks = document.createElement("div");
-        menuLinks.id = "links";
-        menuLinks.className = "listlinks"
-        
-        var homeLink = document.createElement("a");
-        homeLink.href = "../docs/index.html";
-        homeLink.text = "Home";
-        homeLink.className = "list-links";
-        menuLinks.appendChild(homeLink);
+    var menuLinks = document.createElement("div");
+    menuLinks.id = "links";
+    menuLinks.className = "listlinks";
+    
+    var homeLinkB = document.createElement("a");
+    homeLinkB.href = "../docs/index.html";
+    homeLinkB.text = "Home";
+    homeLinkB.className = "navbar-button-links";
+    menuLinks.appendChild(homeLinkB);
 
-        var manageModuleLink = document.createElement("a");
-        manageModuleLink.href = "";
-        manageModuleLink.text = "Manage Modules";
-        manageModuleLink.className = "list-links";
-        menuLinks.appendChild(manageModuleLink);
+    var manageModuleLinkB = document.createElement("a");
+    manageModuleLinkB.href = "";
+    manageModuleLinkB.text = "Manage Modules";
+    manageModuleLinkB.className = "navbar-button-links";
+    menuLinks.appendChild(manageModuleLinkB);
         
-        var moduleBiddingLink = document.createElement("a");
-        moduleBiddingLink.href = "";
-        moduleBiddingLink.text = "Module Bidding";
-        moduleBiddingLink.className = "list-links";
-        menuLinks.appendChild(moduleBiddingLink);
+    var moduleBiddingLinkB = document.createElement("a");
+    moduleBiddingLinkB.href = "";
+    moduleBiddingLinkB.text = "Module Bidding";
+    moduleBiddingLinkB.className = "navbar-button-links";
+    menuLinks.appendChild(moduleBiddingLinkB);
 
-        var nusBidPointsLink = document.createElement("a");
-        nusBidPointsLink.href = "";
-        nusBidPointsLink.text = "NUS Bid Points";
-        nusBidPointsLink.className = "list-links";
-        menuLinks.appendChild(nusBidPointsLink);
+    var nusBidPointsLinkB = document.createElement("a");
+    nusBidPointsLinkB.href = "";
+    nusBidPointsLinkB.text = "NUS Bid Points";
+    nusBidPointsLinkB.className = "navbar-button-links";
+    menuLinks.appendChild(nusBidPointsLinkB);
         
-        var tutRegLink = document.createElement("a");
-        tutRegLink.href = "";
-        tutRegLink.text = "Tutorial Registration";
-        tutRegLink.className = "list-links";
-        menuLinks.appendChild(tutRegLink);
+    var tutRegLinkB = document.createElement("a");
+    tutRegLinkB.href = "";
+    tutRegLinkB.text = "Tutorial Registration";
+    tutRegLinkB.className = "navbar-button-links";
+    menuLinks.appendChild(tutRegLinkB);
 
-        var appealLink = document.createElement("a");
-        appealLink.href = "";
-        appealLink.text = "Appeal";
-        appealLink.className = "list-links";
-        menuLinks.appendChild(appealLink);
+    var appealLinkB = document.createElement("a");
+    appealLinkB.href = "";
+    appealLinkB.text = "Appeal";
+    appealLinkB.className = "navbar-button-links";
+    menuLinks.appendChild(appealLinkB);
+    
+    buttonDiv.appendChild(menuLinks);
+    button.onclick = function() { 
+        menuLinks.classList.toggle("show"); 
+        console.log(menuLinks) 
+    };
+    
+    parentDiv.appendChild(logoDiv);
+    parentDiv.appendChild(navbarDiv);
+    parentDiv.appendChild(buttonDiv);
         
-        button.onclick = function() { menuLinks.classList.toggle("show"); console.log(menuLinks) };
-        buttonDiv.appendChild(menuLinks);
-        parentDiv.appendChild(logoDiv);
-        parentDiv.appendChild(buttonDiv);
-        
-    }
 }
 
 function renderBanner(parentId, bannerText){
